c0d092453194de69bf25ee77c192355fdcbc2578
JustDankas <jan-haspel@hotmail.com>
2022-11-25 20:19:55 +0200

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,46 @@
 import * as React from "react";
 import { Component } from "react";
+import { footerLinks } from "../../statics/texts";
+import {
+  AiOutlineTwitter,
+  AiFillFacebook,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { GrFacebookOption } from "react-icons/gr";
 function Footer() {
   return (
-    <footer className="footer">
-      <div>icons</div>
-      <div></div>
+    <footer
+      className="flex flex-col w-100 bg-black text-white
+    justify-center items-center py-7 px-5"
+    >
+      <div className="flex w-[420px] text-[60px] justify-evenly">
+        <div>
+          <AiFillFacebook />
+        </div>
+        <div>
+          <AiOutlineTwitter />
+        </div>
+
+        <div>
+          <AiFillInstagram />
+        </div>
+        <div>
+          <AiFillYoutube />
+        </div>
+      </div>
+      <div className="flex w-[100%] justify-evenly ">
+        {footerLinks.map((linkGroup) => (
+          <div className="flex flex-col justify-start">
+            <h4 className="font-bold text-[40px] my-8">{linkGroup.header}</h4>
+            <div>
+              {linkGroup.links.map((link) => (
+                <div className="text-[32px] font-normal">{link}</div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </footer>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,18 @@
 import * as React from "react";
 import { Component } from "react";
 function Home() {
-  return <div className="text-white">Home</div>;
+  return (
+    <div
+      className="w-[100vw] h-[100vh]"
+      style={{
+        backgroundImage:
+          "url(https://media.discordapp.net/attachments/908061505956757527/1045435685533929492/Welcome_-_Raw_Food_Chef.jpg?width=1025&height=683)",
+        objectFit: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "100%",
+      }}
+    ></div>
+  );
 }
 
 export default Home;
