6603149dcdab1c8d0529f0541db850bec4bcbee6
JustDankas <jan-haspel@hotmail.com>
2022-11-25 18:50:06 +0200

    tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      {import.meta.env.VITE_API}
       <Routes>
-        <Route path="/"></Route>
+        <Route path="/" element={<Home />}></Route>
       </Routes>
+      <Footer />
     </div>
   );
 }
