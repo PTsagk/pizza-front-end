74e14bc5055c0a44d9e9f88b45df6d3d7cba3e97
JustDankas <jan-haspel@hotmail.com>
2022-11-25 20:27:53 +0200

    bg

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,19 @@
 import * as React from "react";
 import { Component } from "react";
+import BG from "../../assets/veggiesBG.jpg";
 function Home() {
   return (
-    <div
-      className="w-[100vw] h-[100vh]"
-      style={{
-        backgroundImage:
-          "url(https://media.discordapp.net/attachments/908061505956757527/1045435685533929492/Welcome_-_Raw_Food_Chef.jpg?width=1025&height=683)",
-        objectFit: "cover",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "100%",
-      }}
-    ></div>
+    <div className="w-[100vw] h-[100vh]" style={{}}>
+      <img
+        src={BG}
+        alt=""
+        className="absolute top-0 left-0
+      w-[100%] h-[100%]"
+        style={{
+          objectFit: "cover",
+        }}
+      />
+    </div>
   );
 }
 
