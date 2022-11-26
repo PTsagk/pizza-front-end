7015218e2218c04bf35e7ad8298287451f2a7207
PTsag100 <tsagouris500@gmail.com>
2022-11-27 00:42:51 +0200

    pizza carousel

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,28 @@
-import BG from "../../assets/veggiesBG.jpg";
 import "./home.css";
+import pizza from "../../assets/pizza3.png";
 function Home() {
   return (
-    <div className="w-[100%] h-[100%] home-bg">
-      {/* <img
-        src={BG}
-        alt=""
-        className="absolute top-0 left-0 w-[100%] h-[100%]"
-        style={{
-          objectFit: "cover",
-          zIndex: -1,
-        }}
-      /> */}
+    <div className="h-[200vh] relative">
+      <div className="home-bg h-[100vh] relative">
+        {/* <div className="carousel-container absolute top-0">
+          <img
+            src={pizza}
+            alt=""
+            className="w-[80%] absolute top-[50%]"
+            style={{ border: "1px solid white" }}
+          />
+        </div> */}
+      </div>
+      <div className="absolute w-[100%] h-[100%] top-0 left-0 flex flex-col">
+        <div className="w-[100%] h-[40%]"></div>
+        <div className="w-[100%] h-[20%]">
+          <div className="carousel-container absolute top-0">
+            <img src={pizza} alt="" className="w-[70%] absolute" />
+          </div>
+        </div>
+        <div className="w-[100%] h-[40%]"></div>
+      </div>
+      <div className="red-body"></div>
     </div>
   );
 }
