c82e38d001c2626ee9b1776d290453eae641da5d
JustDankas <jan-haspel@hotmail.com>
2022-11-26 18:34:52 +0200

    nav

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Component } from "react";
+function ProductCard() {
+  return (
+    <div>
+      <div></div>
+      <img src="" alt="" />
+      <p></p>
+      <button>Add to Card</button>
+    </div>
+  );
+}
+
+export default ProductCard;
--- src/pages/Pizzas/Pizzas.tsx
+++ src/pages/Pizzas/Pizzas.tsx
@@ -1,6 +1,68 @@
 import * as React from "react";
 import { Component } from "react";
 import MyVeggiesBG from "../../assets/MyVeggies.png";
+
+const DATA = [
+  {
+    category: "Vegeterian",
+    pizzas: [
+      {
+        name: "Veggie",
+        image: "https://www.dominos.gr/gallery/fmobile/1532medium.png",
+        description: `100% Μοτσαρέλα, σάλτσα ντομάτας,
+         πεππερόνι, πικάντικο μοσχάρι, φρέσκα μανιτάρια. 
+         Σε λεπτή ζύμη, με 41 εκ. διάμετρο και κομμένη 
+         σε 6 μεγάλα κομμάτια που διπλώνεις στη μέση!`,
+      },
+      {
+        name: "Margarita",
+        image: "https://www.dominos.gr/gallery/fmobile/1532medium.png",
+        description: `100% Μοτσαρέλα, σάλτσα ντομάτας,
+         πεππερόνι, πικάντικο μοσχάρι, φρέσκα μανιτάρια. 
+         Σε λεπτή ζύμη, με 41 εκ. διάμετρο και κομμένη 
+         σε 6 μεγάλα κομμάτια που διπλώνεις στη μέση!`,
+      },
+      {
+        name: "Ham and Bacon",
+        image: "https://www.dominos.gr/gallery/fmobile/1532medium.png",
+        description: `100% Μοτσαρέλα, σάλτσα ντομάτας,
+         πεππερόνι, πικάντικο μοσχάρι, φρέσκα μανιτάρια. 
+         Σε λεπτή ζύμη, με 41 εκ. διάμετρο και κομμένη 
+         σε 6 μεγάλα κομμάτια που διπλώνεις στη μέση!`,
+      },
+    ],
+  },
+  {
+    category: "Meat Lover's",
+    pizzas: [
+      {
+        name: "Veggie",
+        image: "https://www.dominos.gr/gallery/fmobile/1532medium.png",
+        description: `100% Μοτσαρέλα, σάλτσα ντομάτας,
+         πεππερόνι, πικάντικο μοσχάρι, φρέσκα μανιτάρια. 
+         Σε λεπτή ζύμη, με 41 εκ. διάμετρο και κομμένη 
+         σε 6 μεγάλα κομμάτια που διπλώνεις στη μέση!`,
+      },
+      {
+        name: "Ham",
+        image: "https://www.dominos.gr/gallery/fmobile/1532medium.png",
+        description: `100% Μοτσαρέλα, σάλτσα ντομάτας,
+         πεππερόνι, πικάντικο μοσχάρι, φρέσκα μανιτάρια. 
+         Σε λεπτή ζύμη, με 41 εκ. διάμετρο και κομμένη 
+         σε 6 μεγάλα κομμάτια που διπλώνεις στη μέση!`,
+      },
+      {
+        name: "Pepperoni",
+        image: "https://www.dominos.gr/gallery/fmobile/1532medium.png",
+        description: `100% Μοτσαρέλα, σάλτσα ντομάτας,
+         πεππερόνι, πικάντικο μοσχάρι, φρέσκα μανιτάρια. 
+         Σε λεπτή ζύμη, με 41 εκ. διάμετρο και κομμένη 
+         σε 6 μεγάλα κομμάτια που διπλώνεις στη μέση!`,
+      },
+    ],
+  },
+  {},
+];
 function Pizzas() {
   return (
     <div className="w-[100%] h-[100%] relative">
