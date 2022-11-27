a5650881b223059bb2b206a50a16cbca75139bec
PTsag100 <tsagouris500@gmail.com>
2022-11-27 16:21:43 +0200

    finished pizza carousel

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,28 +1,53 @@
-import "./home.css";
+import "./Home.css";
 import pizza from "../../assets/pizza3.png";
+import { MdArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
+import { useState } from "react";
 function Home() {
+  const [position, setPosition] = useState(0);
+  function moveRight() {
+    if (position > -200) {
+      let pos = position;
+      pos -= 100;
+      setPosition(pos);
+    }
+  }
+  function moveLeft() {
+    if (position < 0) {
+      let pos = position;
+      pos += 100;
+      setPosition(pos);
+    }
+  }
+
   return (
-    <div className="h-[200vh] relative">
-      <div className="home-bg h-[100vh] relative">
-        {/* <div className="carousel-container absolute top-0">
-          <img
-            src={pizza}
-            alt=""
-            className="w-[80%] absolute top-[50%]"
-            style={{ border: "1px solid white" }}
-          />
-        </div> */}
-      </div>
-      <div className="absolute w-[100%] h-[100%] top-0 left-0 flex flex-col">
-        <div className="w-[100%] h-[40%]"></div>
-        <div className="w-[100%] h-[20%]">
-          <div className="carousel-container absolute top-0">
-            <img src={pizza} alt="" className="w-[70%] absolute" />
+    <div className="home-page">
+      <div className="body home-bg">
+        <div className="pizza-info">
+          <button onClick={() => moveLeft()}>
+            <MdArrowBackIosNew className="arrow"></MdArrowBackIosNew>
+          </button>
+          <div className="flex flex-col">
+            <h2>Pizza Special</h2>
+            <p>Tomato sauce, Gouda, Peperoni, Bacon, Green pepper, Mushrooms</p>
           </div>
+          <button onClick={() => moveRight()}>
+            <MdArrowForwardIos className="arrow"></MdArrowForwardIos>
+          </button>
         </div>
-        <div className="w-[100%] h-[40%]"></div>
       </div>
-      <div className="red-body"></div>
+
+      <div className={`pizza-carousel left-[${position}%]`}>
+        <div className="pizza-1-container w-[100vw] flex justify-center">
+          <img src={pizza} alt="" className="w-[90%]" />
+        </div>
+        <div className="pizza-2-container w-[100vw] flex justify-center">
+          <img src={pizza} alt="" className="w-[90%]" />
+        </div>
+        <div className="pizza-3-container w-[100vw] flex justify-center">
+          <img src={pizza} alt="" className="w-[90%]" />
+        </div>
+      </div>
+      <div className="body red-body"></div>
     </div>
   );
 }
