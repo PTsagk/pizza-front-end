8465274f13250bf0b551cf629ee5386e1a837884
JustDankas <jan-haspel@hotmail.com>
2022-11-28 02:06:25 +0200

    animations and offers

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,23 +1,38 @@
-import "./Home.css";
+import "./home.css";
 import pizza from "../../assets/pizza3.png";
 import { MdArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { homeOffers } from "../../statics/texts";
 function Home() {
   const [position, setPosition] = useState(0);
+  const [canClick, setCanClick] = useState(true);
+
   function moveRight() {
+    if (!canClick) return;
     if (position > -200) {
       let pos = position;
       pos -= 100;
       setPosition(pos);
+      setCanClick(false);
     }
   }
   function moveLeft() {
+    if (!canClick) return;
     if (position < 0) {
       let pos = position;
       pos += 100;
       setPosition(pos);
+      setCanClick(false);
     }
   }
+
+  useEffect(() => {
+    if (!canClick) {
+      setTimeout(() => {
+        setCanClick(true);
+      }, 1300);
+    }
+  }, [canClick]);
 
   return (
     <div className="home-page">
@@ -39,16 +54,39 @@ function Home() {
 
       <div className={"pizza-carousel"} style={{ left: position + "%" }}>
         <div className="pizza-1-container w-[100vw] flex justify-center">
-          <img src={pizza} alt="" className="w-[80%]" />
+          <img src={pizza} alt="pizza" className="w-[80%]" />
         </div>
         <div className="pizza-2-container w-[100vw] flex justify-center">
-          <img src={pizza} alt="" className="w-[80%]" />
+          <img src={pizza} alt="pizza" className="w-[80%]" />
         </div>
         <div className="pizza-3-container w-[100vw] flex justify-center">
-          <img src={pizza} alt="" className="w-[80%]" />
+          <img src={pizza} alt="pizza" className="w-[80%]" />
         </div>
       </div>
-      <div className="body red-body"></div>
+      <div className="body red-body relative">
+        <div
+          className="w-[100%] h-[50%] absolute top-[50%]
+        flex justify-evenly  items-center lg:flex-row flex-col"
+        >
+          {homeOffers.map((offer) => (
+            <div className="flex flex-col items-start text-white">
+              <div
+                className="flex justify-start items-center
+              xxl:text-[54px] font-bold text-[42px]"
+              >
+                <h3>{offer.header}</h3>
+                <span className="ml-2">{offer.headerIcon}</span>
+              </div>
+              <div className="xxl:text-[32px] text-[22px] font-semibold mt-10 mb-5">
+                {offer.timeRange}
+              </div>
+              <div className="xxl:text-[24px] text-[18px] font-normal">
+                {offer.description}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/statics/texts.tsx
@@ -0,0 +1,47 @@
+import { CgStopwatch } from "react-icons/cg";
+import { FaConciergeBell } from "react-icons/fa";
+import { MdOutlineToys } from "react-icons/md";
+export const footerLinks = [
+  {
+    header: "Contact",
+    links: ["Email", "Phone", "Adress"],
+  },
+  {
+    header: "Menu",
+    links: ["Pizzas", "Drinks", "Desserts"],
+  },
+  {
+    header: "Payment",
+    links: ["Visa", "Mastercard", "Paypal"],
+  },
+  {
+    header: "Offers",
+    links: ["Happy Hour", "Online deals", "Kids Menu"],
+  },
+  {
+    header: "Stores",
+    links: ["Kalamata", "Piraeus", "Agios Dimirios", "Brylissia"],
+  },
+];
+
+export const homeOffers = [
+  {
+    header: "Happy Hour",
+    headerIcon: <CgStopwatch />,
+    timeRange: "Monday-Thursday",
+    description: "Add 2 or more large or giga pizzas and get 20% off!",
+  },
+  {
+    header: "-10% Online",
+    headerIcon: <FaConciergeBell />,
+    timeRange: "Monday-Friday",
+    description: "Get -10% by ordering online, through our website or our app!",
+  },
+  {
+    header: "Kids Menu",
+    headerIcon: <MdOutlineToys />,
+    timeRange: "Always",
+    description:
+      "Enjoy playfull meals with healthy options for kids and toys included!",
+  },
+];
