6e45772aefcd5a30d297ef74b4acb0e883f7a9c6
JustDankas <jan-haspel@hotmail.com>
2022-12-17 18:52:42 +0200

    address context

--- src/Context/addressContext.tsx
+++ src/Context/addressContext.tsx
@@ -2,12 +2,25 @@ import { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
 const AddressContext = createContext({});
 
+interface IAddress {
+  address: string;
+  addressNumber: string;
+  city: string;
+  id: string;
+  phoneNumber: string;
+  user_id: string;
+}
+
+interface IAddressContext {
+  addresses: IAddress[];
+}
+
 export function useAddressContext() {
   return useContext(AddressContext);
 }
 
-function AddressProvider({ children }) {
-  const [address, setAddress] = useState([]);
+function AddressProvider({ children }: { children: React.ReactNode }) {
+  const [addresses, setAddresses] = useState<IAddress[]>([]);
 
   useEffect(() => {
     axios.defaults.withCredentials = true;
@@ -15,15 +28,14 @@ function AddressProvider({ children }) {
     axios
       .get(`${import.meta.env.VITE_API}/address`)
       .then((res) => {
-        setAddress([...res.data]);
+        setAddresses([...res.data]);
       })
       .catch((e) => console.log(e));
   }, []);
   return (
     <AddressContext.Provider
       value={{
-        address,
-        setAddress,
+        addresses,
       }}
     >
       {children}
